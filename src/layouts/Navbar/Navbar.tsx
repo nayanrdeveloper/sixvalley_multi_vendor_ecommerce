b13613c551f1ad19e7b371cfb651182a9fda9ca4
nayanrdeveloper <nayanrdeveloper@gmail.com>
2023-06-14 17:36:41 +0530

    init project inilization

---
+++ src/layouts/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div className='flex justify-between'>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
